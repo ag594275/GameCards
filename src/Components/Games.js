c0a16286f76222772d4b7c3609d5f5ce4246ab1c
Abhishek Gupta <ag594275@gmail.com>
2024-05-12 02:45:33 +0530

    Updated

--- src/Components/Games.js
+++ src/Components/Games.js
@@ -5,7 +5,7 @@ function Games({games,removeGames}){
     return(
         <div className='container'>
             <div>
-                <h1 className='title'>Play with joy</h1>
+                <h1 className='title'>Play with fun</h1>
             </div>
             <div className='cards'>
                 {
